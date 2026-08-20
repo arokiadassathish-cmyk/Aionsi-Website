e649bddf5ec6424d69e173e2c3d5d984ddde81b5
arokiadassathish-cmyk <arokiadas.sathish@gmail.com>
2026-08-20 15:57:53 +0530

    add confirmed AionSi client roster

---
+++ src/data/customers.ts
@@ -0,0 +1,18 @@
+export const customerLogos = [
+  { name: 'C SPEED', logo: '/images/customers/c-speed.png' },
+  { name: 'Cadence', logo: '/images/customers/cadence.png' },
+  { name: 'Samsung', logo: '/images/customers/samsung.png' },
+  { name: 'Synopsys', logo: '/images/customers/synopsys.png' },
+  { name: 'HCL', logo: '/images/customers/hcl.png' },
+  { name: 'Aeva', logo: '/images/customers/aeva.png' },
+  { name: 'Chipright', logo: '/images/customers/chipright.png' },
+  { name: 'Nexpi', logo: '/images/customers/nexpi.png' },
+  { name: 'SanDisk', logo: '/images/customers/sandisk.png' },
+  { name: 'L&T Technology Services', logo: '/images/customers/ltts.png' },
+  { name: 'Tech Mahindra', logo: '/images/customers/tech-mahindra.png' },
+  { name: 'NXP', logo: '/images/customers/nxp.png' },
+  { name: 'ChipLogic', logo: '/images/customers/chiplogic.png' },
+  { name: 'Nordea', logo: '/images/customers/nordea.png' },
+];
+
+export const customerStatement = 'Trusted by 15+ customers across semiconductor, consumer, automotive, networking and AI accelerator segments.';
